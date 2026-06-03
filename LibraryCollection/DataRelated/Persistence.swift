c46c4ddfd14f424557d8a1c4a53679519a006539
Elizabeth Rose <rose.eg@gmail.com>
2026-06-02 22:07:43 -0400

    Update persistence controller and App

--- LibraryCollection/DataRelated/Persistence.swift
+++ LibraryCollection/DataRelated/Persistence.swift
@@ -1,31 +1,59 @@
-//
-//  Persistence.swift
-//  TestProject
-//
-//  Created by Elizabeth Rose on 6/1/26.
-//
+/*
+See the LICENSE.txt file for this sample’s licensing information.
 
+Abstract:
+A class that sets up the Core Data stack.
+*/
+import Foundation
 import CoreData
 
-struct PersistenceController {
+class PersistenceController {
+    
     static let shared = PersistenceController()
-
-    @MainActor
-    let container: NSPersistentCloudKitContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentCloudKitContainer(name: "DataModel")
-        /*
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+    
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
+                        
+        let container = NSPersistentCloudKitContainer(name: "DataModel", managedObjectModel: self.persistentContainer.managedObjectModel)
+        
+        // Main context for UI operations (runs on main thread)
+        var viewContext: NSManagedObjectContext {
+            container.viewContext
         }
-         */
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        viewContext.undoManager = nil
+
+        // Create a store description for a CloudKit-backed local store
+        let cloudStoreLocation = URL(fileURLWithPath: "Bundle.main.applicationSupportDirectory")
+        let cloudStoreDescription = NSPersistentStoreDescription(url: cloudStoreLocation)
+        
+        cloudStoreDescription.configuration = "Default"
+        cloudStoreDescription.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.com.RiverThree.LC")
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                fatalError("Core Data failed to load: \(error.localizedDescription)")
             }
         })
-        container.viewContext.automaticallyMergesChangesFromParent = true
+
+#if DEBUG
+        print(container.persistentStoreDescriptions[0].url?.absoluteURL.path.removingPercentEncoding as Any)
+#endif
+
+        return container
+        
+    }()
+    
+    func save() {
+        
+        let context = persistentContainer.viewContext
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+                context.refreshAllObjects()
+            } catch {
+                print("Error on saving the moc context in PersistenceController")
+            }
+        }
     }
 }
--- LibraryCollection/LibraryCollectionApp.swift
+++ LibraryCollection/LibraryCollectionApp.swift
@@ -17,11 +17,20 @@ struct LibraryCollectionApp: App {
             Item.self
         ])
         
+        //define the path for the local installation
         let storeURL = URL.applicationSupportDirectory.appending(path: "DataModel.sqlite")
-        let config = ModelConfiguration(schema: schema, url: storeURL, allowsSave: true)
         
+        //define two configurations - one for the local store and one for the cloudKit
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false, allowsSave: true)
+
         let cloudKitContainer = ModelConfiguration("DataModel.sqlite", cloudKitDatabase: .private("DataModel"))
         
+        //now try to construct or open them here
+        do {
+            return try ModelContainer(for: schema, configurations: modelConfiguration)
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
         do {
             return try ModelContainer(for: schema, configurations: cloudKitContainer)
         } catch {
